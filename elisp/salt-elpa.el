5436a4f1294ae21e8bcc020fbf677e507ae2324b
cervelleless <cervelleless@gmail.com>
2020-12-04 23:50:15 +0800

    replace quelpa with straight

--- elisp/salt-elpa.el
+++ elisp/salt-elpa.el
@@ -19,41 +19,62 @@
 ;;
 ;;; Code:
 
-;; melpa
-;; (require 'package)
-(setq package-user-dir (concat user-emacs-directory "quelpa/"))
-;; (add-to-list 'load-path (concat package-user-dir "use-package-20201110.2133/"))
-;; (add-to-list 'load-path (concat package-user-dir "bind-key-20200805.1727/"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-;;                          ;;  Comment/uncomment this line to enable MELPA Stable if desired.
-;;                          ;;  ("melpa-stable" . "https://stable.melpa.org/packages/")
-                          ("org" . "https://orgmode.org/elpa/")
-                          ("elpa" . "https://elpa.gnu.org/packages/")))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+
+(setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+(setq use-package-always-ensure t)
+(use-package git) ;; ensure we can install from git sources
+
+;; test straight.el
+;; ;; melpa
+;; ;; (require 'package)
+;; (setq package-user-dir (concat user-emacs-directory "quelpa/"))
+;; ;; (add-to-list 'load-path (concat package-user-dir "use-package-20201110.2133/"))
+;; ;; (add-to-list 'load-path (concat package-user-dir "bind-key-20200805.1727/"))
+;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;; ;;                          ;;  Comment/uncomment this line to enable MELPA Stable if desired.
+;; ;;                          ;;  ("melpa-stable" . "https://stable.melpa.org/packages/")
+;;                           ("org" . "https://orgmode.org/elpa/")
+;;                           ("elpa" . "https://elpa.gnu.org/packages/")))
+;; ;; (package-initialize)
+
+;; (eval-when-compile
+;;   (require 'package)
+;;   (package-initialize)
+;;   (unless (package-installed-p 'use-package)
+;;     (package-refresh-contents)
+;;     (package-install 'use-package))
+;;   (require 'use-package)
+;;   (setq use-package-always-ensure t))
+
+;; (eval-when-compile
+;;   (require 'use-package))
 ;; (package-initialize)
+;; (require 'bind-key)
 
-(eval-when-compile
-  (require 'package)
-  (package-initialize)
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-  (require 'use-package)
-  (setq use-package-always-ensure t))
+;; (use-package quelpa
+;;   :defer t
+;;   :init (setq quelpa-update-melpa-p nil)
+;;   :commands quelpa)
 
-(eval-when-compile
-  (require 'use-package))
-(package-initialize)
-(require 'bind-key)
-
-(use-package quelpa
-  :defer t
-  :init (setq quelpa-update-melpa-p nil)
-  :commands quelpa)
-
-(use-package quelpa-use-package
-  :defer t
-  :init
-  (setq use-package-ensure-function 'quelpa))
+;; (use-package quelpa-use-package
+;;   :defer t
+;;   :init
+;;   (setq use-package-ensure-function 'quelpa))
 
 (provide 'salt-elpa)
 ;;; salt-elpa.el ends here
