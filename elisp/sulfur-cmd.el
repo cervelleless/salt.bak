c764d3fee5a5630c6b8925259160302d148f9828
cervelleless <cervelleless@gmail.com>
2020-12-10 08:09:48 +0800

    net start yet. will move some code in sulfur here.

---
+++ elisp/sulfur-cmd.el
@@ -0,0 +1,24 @@
+;;; sulfur-cmd.el --- commands -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 cervelleless
+;;
+;; Author: cervelleless <http://github.com/cervelleless>
+;; Maintainer: cervelleless <cervelleless@gmail.com>
+;; Created: November 27, 2020
+;; Modified: November 27, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/felix/sulfur
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Vi emulator. It's rudimentary, but enough.
+;;
+;;; Code:
+
+
+(provide 'salfur-cmd)
+;;; sulfur-cmd.el ends here.
