9e3085f55e73a755ddb7334425ad01eb53237b71
cervelleless <cervelleless@gmail.com>
2020-12-20 00:02:11 +0800

    replace doom-theme with immaterial-theme

--- elisp/salt-ui.el
+++ elisp/salt-ui.el
@@ -24,35 +24,48 @@
 ;; (require 'bind-key)
 
 ;; theme
-(use-package doom-themes
-  :init (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-              doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  :config
-  (load-theme 'doom-one t))
+;;(use-package doom-themes
+;;  :init (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;              doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;  :config
+;;  (load-theme 'doom-one t))
+;;
 
-(use-package minions
-  :defer t
-  :hook (doom-modeline-mode . minions-mode)
-  :custom
-  (minions-mode-line-lighter ""))
+(use-package immaterial-theme
+    :ensure t
+    :config
+    (load-theme 'immaterial-dark t))
 
-(use-package doom-modeline
-  :defer t
-  :hook (after-init . doom-modeline-mode)
-  :custom-face
-  (mode-line ((t (:height 1.0))))
-  (mode-line-inactive ((t (:height 1.0))))
-  :init
-  (setq doom-modeline-height 15
-        doom-modeline-bar-width 6
-        doom-modeline-lsp t
-        doom-modeline-github nil
-        doom-modeline-mu4e nil
-        doom-modeline-irc nil
-        doom-modeline-minor-modes t
-        doom-modeline-persp-name nil
-        doom-modeline-buffer-file-name-style 'truncate-except-project
-        doom-modeline-major-mode-icon nil))
+
+;; 
+;; (use-package minions
+;;   :defer t
+;;   :hook (doom-modeline-mode . minions-mode)
+;;   :custom
+;;   (minions-mode-line-lighter ""))
+;; 
+;; (use-package doom-modeline
+;;   :defer t
+;;   :hook (after-init . doom-modeline-mode)
+;;   :custom-face
+;;   (mode-line ((t (:height 1.0))))
+;;   (mode-line-inactive ((t (:height 1.0))))
+;;   :init
+;;   (setq doom-modeline-height 15
+;;         doom-modeline-bar-width 6
+;;         doom-modeline-lsp t
+;;         doom-modeline-github nil
+;;         doom-modeline-mu4e nil
+;;         doom-modeline-irc nil
+;;         doom-modeline-minor-modes t
+;;         doom-modeline-persp-name nil
+;;         doom-modeline-buffer-file-name-style 'truncate-except-project
+;;         doom-modeline-major-mode-icon nil))
+
+;; (use-package material-theme
+;;   :config
+;;   (load-theme 'material t))
+
 
 
 
